ea6e7492a328bd68832c8dad05b23af70a313f31
Dominque Douglas <douglas@dkey.org>
2019-03-19 10:42:11 -0500

    client-side-tools update

--- copy-client-software.sh
+++ copy-client-software.sh
@@ -3,6 +3,7 @@
 #
 export HSM_SOFTWARE_PATH="client-software"
 export HSM_SOFTWARE_BUILD="sw/client-side-tools"
+export HSM_SOFTWARE_BUILD_BIN="${HSM_SOFTWARE_BUILD}/bin"
 export DKEY_INSTALL="install"
 export HSM_PKCS11_BUILD="sw/pkcs11"
 mkdir -p $HSM_SOFTWARE_PATH 
@@ -11,6 +12,6 @@ rm -rf $HSM_SOFTWARE_PATH/*
 cp -R $DKEY_INSTALL/* $HSM_SOFTWARE_PATH
 
 cp -R $HSM_SOFTWARE_BUILD/findHSM $HSM_SOFTWARE_PATH
-cp -R $HSM_SOFTWARE_BUILD/dks_setup_console $HSM_SOFTWARE_PATH
+cp -R $HSM_SOFTWARE_BUILD_BIN/dks_setup_console $HSM_SOFTWARE_PATH
 cp -R $HSM_PKCS11_BUILD/libcryptechdks-pkcs11.so $HSM_SOFTWARE_PATH
 tar cvfz dks-client-software.tgz $HSM_SOFTWARE_PATH/
